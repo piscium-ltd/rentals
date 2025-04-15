5a8747f610afee9a281f8ed50da29839fd736240
PhilomenaMusili <philomenamusili2@gmail.com>
2025-04-15 13:57:43 +0300

    feat: doctype

--- rentals/rentals/doctype/tenant/tenant.js
+++ rentals/rentals/doctype/tenant/tenant.js
@@ -1,8 +1,12 @@
 // Copyright (c) 2025, Piscium Solutions LTD and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Tenant", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Tenant", {
+	refresh(frm) {
+        frm.set_query("unit", () => ({
+            filters:{
+                property: frm.doc.property
+            },
+        }));
+	},
+});
