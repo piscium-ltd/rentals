3d72faca7bb5826018a468c31976def21e5a2f95
Kalutu <kalutudaniel@gmail.com>
2025-07-16 23:01:16 +0300

    fix: landlord doctype changes

---
+++ rentals/rentals/doctype/landlord_group/landlord_group.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Piscium Solutions LTD and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.utils.nestedset import NestedSet
+
+
+class LandlordGroup(NestedSet):
+	pass
