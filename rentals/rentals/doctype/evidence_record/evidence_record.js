d99efa8b7ff0c593d6ae10def8f9333f1b3adddb
PhilomenaMusili <philomenamusili2@gmail.com>
2026-09-10 23:27:49 +0300

    feat:auto-generate full name

--- rentals/rentals/doctype/evidence_record/evidence_record.js
+++ rentals/rentals/doctype/evidence_record/evidence_record.js
@@ -1,8 +1,27 @@
 // Copyright (c) 2026, Piscium Solutions LTD and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Evidence Record", {
-// 	refresh(frm) {
 
-// 	},
-// });
+frappe.ui.form.on('Evidence Record', {
+    surname_as_document: function(frm) {
+        set_full_name(frm);
+    },
+
+    first_name_as_document: function(frm) {
+        set_full_name(frm);
+    },
+
+    middle_names_as_document: function(frm) {
+        set_full_name(frm);
+    }
+});
+
+function set_full_name(frm) {
+    let names = [
+        frm.doc.first_name_as_document,
+        frm.doc.middle_names_as_document,
+        frm.doc.surname_as_document
+    ].filter(Boolean);
+
+    frm.set_value('full_name_as_document', names.join(' '));
+}
