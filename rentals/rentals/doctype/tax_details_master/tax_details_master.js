43f60eddf82069bf98cb4609e7a5a3d3d50b79e2
PhilomenaMusili <philomenamusili2@gmail.com>
2026-09-22 20:27:28 +0300

    title case

--- rentals/rentals/doctype/tax_details_master/tax_details_master.js
+++ rentals/rentals/doctype/tax_details_master/tax_details_master.js
@@ -1,8 +1,15 @@
 // Copyright (c) 2026, Piscium Solutions LTD and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Tax Details Master", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Tax Details Master", {
+	name1(frm) {
+		if (frm.doc.name1) {
+			frm.set_value(
+				"name1",
+				frm.doc.name1
+					.toLowerCase()
+					.replace(/\b\w/g, char => char.toUpperCase())
+			);
+		}
+	},
+});
