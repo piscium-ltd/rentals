3c44d73a095ea2cf0172f2d0a403d94639df6264
PhilomenaMusili <philomenamusili2@gmail.com>
2026-09-22 20:24:19 +0300

    Title Case

--- rentals/rentals/doctype/org_reg_document_type/org_reg_document_type.js
+++ rentals/rentals/doctype/org_reg_document_type/org_reg_document_type.js
@@ -1,8 +1,15 @@
 // Copyright (c) 2026, Piscium Solutions LTD and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Org Reg Document Type", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Org Reg Document Type", {
+	document_type(frm) {
+		if (frm.doc.document_type) {
+			frm.set_value(
+				"document_type",
+				frm.doc.document_type
+					.toLowerCase()
+					.replace(/\b\w/g, char => char.toUpperCase())
+			);
+		}
+	},
+});
